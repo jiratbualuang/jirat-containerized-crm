f10c6da57d5d811d02af8d95209ec31de40c1e80
jiratbualuang <jirat.bua@dome.tu.ac.th>
2024-04-10 03:58:47 +0000

    Initial CRM prject setup with security and Docker

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import jsonwebtoken from "jsonwebtoken";
 import routes from './src/routes/crmRoutes.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3020;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
